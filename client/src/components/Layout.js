9d1b48c0d73ec2123ec66e7328314db80d81907a
noumantechie <nomanyousaf712@gmail.com>
2025-11-05 04:21:30 +0500

    Image updater

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -16,7 +16,7 @@ function Layout({ children }) {
           </div>
         </div>
       </header>
-      <AnimatedBanner message="Welcome to Nouman Yousaf and Kanzal Qalandri 🚀" />
+      <AnimatedBanner message="Welcome to Nouman Yousaf🚀" />
       <div className="app-body">
         <aside className="sidebar slide-in-left">
           <h3>Connect</h3>
